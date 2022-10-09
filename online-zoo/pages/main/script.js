30c4d7fa706897343635c534145537df03388f6b
Anastasia <nastia.shrai@gmail.com>
2022-10-09 19:06:44 +0300

    feat: implement carousel logic on main page

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,9 +1,72 @@
+const animalArray = [
+	{
+		id: 1,
+		name: 'giant Pandas',
+		img: '../../assets/images/pets-panda.png',
+		location: 'Native to Southwest China',
+		mealIcon: '../../assets/icons/pets-banana-bamboo_icon.svg',
+	},
+	{
+		id: 2,
+		name: 'Eagles',
+		img: '../../assets/images/pets-eagle.png',
+		location: 'Native to South America',
+		mealIcon: '../../assets/icons/pets-meat-fish_icon.svg',
+	},
+	{
+		id: 3,
+		name: 'Gorillas',
+		img: '../../assets/images/pets-gorilla.png',
+		location: 'Native to Congo',
+		mealIcon: '../../assets/icons/pets-banana-bamboo_icon.svg',
+	},
+	{
+		id: 4,
+		name: 'Two-toed Sloth',
+		img: '../../assets/images/pets-sloth.png',
+		location: 'Mesoamerica, South America',
+		mealIcon: '../../assets/icons/pets-banana-bamboo_icon.svg',
+	},
+	{
+		id: 5,
+		name: 'cheetahs',
+		img: '../../assets/images/pets-cheetah.png',
+		location: 'Native to Africa',
+		mealIcon: '../../assets/icons/pets-meat-fish_icon.svg',
+	},
+	{
+		id: 6,
+		name: 'Penguins',
+		img: '../../assets/images/pets-penguin.png',
+		location: 'Native to Antarctica',
+		mealIcon: '../../assets/icons/pets-meat-fish_icon.svg',
+	},
+	{
+		id: 7,
+		name: 'Alligators',
+		img: '../../assets/images/pets-alligator.png',
+		location: 'Native to Southeastern U. S.',
+		mealIcon: '../../assets/icons/pets-meat-fish_icon.svg',
+	},
+	{
+		id: 8,
+		name: 'Koalas',
+		img: '../../assets/images/pets-koala.png',
+		location: 'Native to Australia',
+		mealIcon: '../../assets/icons/pets-banana-bamboo_icon.svg',
+	},
+];
+
 const burgerIcon = document.querySelector('.burger__menu');
 const headerLogo = document.querySelector('.logo');
 const navigationMenu = document.querySelector('.navigation__list');
 const navigationItems = document.querySelectorAll('.navigation__list-item');
 const overlay = document.createElement('div');
 overlay.classList.add('overlay');
+
+const petsContainer = document.querySelector('.pets__cards');
+const prevPetsPage = document.querySelector('.prev-btn');
+const nextPetsPage = document.querySelector('.next-btn');
 
 function open() {
 	navigationMenu.classList.remove('hide');
@@ -26,3 +89,95 @@ burgerIcon.addEventListener('click', () => {
 });
 
 overlay.addEventListener('click', close);
+
+let petsData = [...animalArray];
+let fullAnimalList = [];
+
+function createPets() {
+	fullAnimalList = (() => {
+		let tempArr = [];
+
+		for (let i = 0; i < 6; i++) {
+			const newPets = petsData;
+
+			for (let j = newPets.length; j > 0; j--) {
+				let randInd = Math.floor(Math.random() * j);
+				const randElem = petsData.splice(randInd, 1)[0];
+				newPets.push(randElem);
+			}
+
+			tempArr = [...newPets];
+		}
+		return tempArr;
+	})();
+	fullAnimalList = sortPetsList(fullAnimalList);
+
+	createPetsCards(fullAnimalList);
+}
+createPets();
+
+function createPetsCards(list) {
+	petsContainer.innerHTML += createElements(list);
+}
+
+function createElements(petsList) {
+	let str = '';
+	for (let i = 0; i < petsList.length; i++) {
+		str += `
+		        <div class="pets__card" data-id="${petsList[i].id}">
+		          <div class="image__wrapper">
+		          	<img class="pets__card-image" src="${petsList[i].img}" alt="${petsList[i].name}">
+		          </div>
+		          <div class="pets__card-content">
+		          	<h5 class="pets__card-title">
+		          	${petsList[i].name}
+		          	</h5>
+		          	<p class="pets__card-desc">
+		          	${petsList[i].location}
+		          	</p>
+		          	<img class="pets__card-icon banana-icon" src="${petsList[i].mealIcon}" alt="">
+		          </div>
+		        </div>
+						`;
+	}
+
+	return str;
+}
+
+function sortPetsList(list) {
+	let unique8List = [];
+	let length = list.length;
+	for (let i = 0; i < length / 8; i++) {
+		const uniqueStepList = [];
+		for (j = 0; j < list.length; j++) {
+			if (uniqueStepList.length >= 8) {
+				break;
+			}
+			const isUnique = !uniqueStepList.some((item) => {
+				return item.name === list[j].name;
+			});
+			if (isUnique) {
+				uniqueStepList.push(list[j]);
+				list.splice(j, 1);
+				j--;
+			}
+		}
+		unique8List = [...unique8List, ...uniqueStepList];
+	}
+	list = unique8List;
+	return list;
+}
+
+let currentPage = 0;
+
+prevPetsPage.addEventListener('click', (e) => {
+	currentPage--;
+	petsContainer.style.top = `calc(0px - ${892 * Math.abs(currentPage)}px)`;
+	createPets();
+});
+
+nextPetsPage.addEventListener('click', () => {
+	currentPage++;
+	petsContainer.style.top = `calc(0px - ${892 * Math.abs(currentPage)}px)`;
+	createPets();
+});
