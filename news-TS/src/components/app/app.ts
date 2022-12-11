fd58b8323bf0b818aceddfadf226e2d5c6294a87
Anastasia <nastia.shrai@gmail.com>
2022-12-11 15:02:51 +0200

    refactor: rewrite using typescript

---
+++ news-TS/src/components/app/app.ts
@@ -0,0 +1,24 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+
+class App {
+    controller: AppController;
+    view: AppView;
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    start() {
+        const sourcesContainer = document.querySelector('.sources') as HTMLElement;
+        sourcesContainer.addEventListener('click', (e: Event) =>
+            this.controller.getNews(e, (data) => this.view.drawNews(data!))
+        );
+        this.controller.getSources((data) => {
+            console.log(data);
+            return this.view.drawSources(data!);
+        });
+    }
+}
+
+export default App;
