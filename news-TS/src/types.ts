077379c70f4e4751f5b7d5e544f593924879c6d2
Anastasia <nastia.shrai@gmail.com>
2022-12-11 14:23:44 +0200

    feat: add types description

--- news-TS/src/types.ts
+++ news-TS/src/types.ts
@@ -1,14 +1,51 @@
 export interface IItemNews {
     urlToImage: string;
     author: string;
-    source: { name: string | null };
+    source: {
+        name: string;
+        id: string;
+    };
     publishedAt: string;
     title: string | null;
     description: string | null;
     url: string;
 }
 
-export interface IItemSource {
-    name: string;
+export interface ISources {
     id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
 }
+
+export interface IData {
+    status: string;
+    totalResults: number;
+    articles: IItemNews[];
+    sources: ISources[];
+}
+
+export enum API_KEY {
+    KEY = 'c467ad4d887946eb9def180f6b8e40dc',
+}
+
+export enum API_URL {
+    URL = 'https://newsapi.org/v2/',
+}
+
+export enum ResponseStatus {
+    AUTH_PROBLEM = 401,
+    NO_RESOURCE_FOUND = 404,
+}
+
+export type GetResp = {
+    endpoint: string;
+    options?: Partial<Options>;
+};
+
+export type Options = {
+    [key: string]: string;
+};
