95bf4c7ff283af6611484311853ed977621a6b01
Anastasia <nastia.shrai@gmail.com>
2023-01-16 22:15:09 +0200

    init: start async-race task

---
+++ async-race/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: 'airbnb-typescript/base',
+  plugins: ['import', 'prettier'],
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
+  rules: {
+    'no-debugger': 'off',
+    'no-console': 0,
+    'class-methods-use-this': 'off',
+    '@typescript-eslint/no-explicit-any': 2,
+  },
+};
