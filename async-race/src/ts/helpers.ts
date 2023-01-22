104548d4446072eb00a6c88004986ec698780e31
Anastasia <nastia.shrai@gmail.com>
2023-01-22 13:58:38 +0200

    feat: add functions to generate random car's name and color

---
+++ async-race/src/ts/helpers.ts
@@ -0,0 +1,20 @@
+const models = ['Tesla', 'Mersedes', 'BMW', 'Toyota', 'Zhiguli', 'Moskwich', 'Opel', 'Aston Martin', 'Porshe'];
+const names = ['Model S', 'CLK', '7', 'Camry', 'Combi', '9', 'Corsa', 'DB9', 'Cayene'];
+
+const getRandomName = () => {
+  const model = models[Math.floor(Math.random() * models.length)];
+  const name = names[Math.floor(Math.random() * names.length)];
+  return `${model} ${name}`;
+};
+
+const getRandomColor = () => {
+  const letters = '0123456789abcdef';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * letters.length)];
+  }
+  return color;
+};
+
+export const generateRandomCars = (count = 100) =>
+  new Array(count).fill(1).map((_) => ({ name: getRandomName(), color: getRandomColor() }));
