0043b722e6ea95e0d23c80e0bd3161da5892f00f
Anastasia <nastia.shrai@gmail.com>
2023-01-23 13:15:39 +0200

    fix: rename file and add types

---
+++ async-race/src/ts/constants.ts
@@ -0,0 +1,4 @@
+export const BASE_URL: string = 'http://127.0.0.1:3000';
+export const GARAGE_URL: string = `${BASE_URL}/garage`;
+export const ENGINE_URL: string = `${BASE_URL}/engine`;
+export const WINNERS_URL: string = `${BASE_URL}/winners`;
--- async-race/src/ts/url.ts
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = 'http://127.0.0.1:3000';
-export const GARAGE_URL = `${BASE_URL}/garage`;
-export const ENGINE_URL = `${BASE_URL}/engine`;
-export const WINNERS_URL = `${BASE_URL}/winners`;
